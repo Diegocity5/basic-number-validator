b2f71b508b5b38900086426b15e0283378352fbe
Diegocity5 <icotecc@gmail.com>
2025-08-27 18:22:06 -0500

    Mejore con addEventListener metodo mas moderno y cambie el orden del codigo

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,47 @@
 //Selecionando elementos del DOM
-const resultsDiv = document.getElementById('results-div');
-const userInput = document.getElementById('user-input');
-const checkBtn = document.getElementById('check-btn');
-const clearBtn = document.getElementById('clear-btn');
+const resultsDiv = document.getElementById("results-div");
+const userInput = document.getElementById("user-input");
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
 
 //Manejando el evento de presion de tecla abajo "Enter".
-document.addEventListener('keypress', (event)=> {
-  if(event.code === "Enter") {
-    checking();
-  }
-})
-
-//Manejando el evento click del boton "CheckBtn".
-checkBtn.onclick = ()=> checking();
-
-//Manejando el evento click del boton "Clear".
-clearBtn.onclick = ()=> resultsDiv.textContent = '';
-
-
-const checking = () =>{
+const checking = () => {
   const value = userInput.value.trim();
-  if(value === ''){
-    alert('Please provide a phone number');
-  }else {
+  if (!value) {
+    alert("Please provide a phone number");
+  } else {
     //Si es un numero hay que validarlo
     const isValid = validateFormatePhoneNumber(value);
     renderNumbers(value, isValid);
   }
-}
+};
 
 //Funcion encargada de validar el numero de telefono.
-const validateFormatePhoneNumber  = (str)=> {
-const  patternNumbers = /^1?\s?(\([\d]{3}\)|[\d]{3})[-\s]?[\d]{3}[-\s]?[\d]{4}$/;
-return patternNumbers.test(str);
-}
+const validateFormatePhoneNumber = (str) => {
+  const patternNumbers =
+    /^1?\s?(\([\d]{3}\)|[\d]{3})[-\s]?[\d]{3}[-\s]?[\d]{4}$/;
+  return patternNumbers.test(str);
+};
 
 //Funcion encargada de renderizar los numeros en el DOM.
 const renderNumbers = (str, isValid) => {
-  const p = document.createElement('p');
-  p.textContent = isValid ? `Valid US number: ${str}` : `Invalid US number: ${str}`;
-  p.className = isValid ? 'valid-number' : 'invalid-number';
+  const p = document.createElement("p");
+  p.textContent = isValid
+    ? `Valid US number: ${str}`
+    : `Invalid US number: ${str}`;
+  p.className = isValid ? "valid-number" : "invalid-number";
   resultsDiv.appendChild(p);
-  userInput.value = '';
-}
+  userInput.value = "";
+};
+
+//Handle Events
+userInput.addEventListener("keydown", (event) => {
+  if (event.code === "Enter") {
+    checking();
+  }
+});
+
+//Manejando el evento click del boton "CheckBtn".
+checkBtn.addEventListener("click", checking);
+//Manejando el evento click del boton "Clear".
+clearBtn.addEventListener("click", () => (resultsDiv.textContent = ""));
